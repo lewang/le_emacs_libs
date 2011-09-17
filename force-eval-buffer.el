a926ce70ce858b7cdc12456d707f203c3cd4a8bf
Le Wang <lewang@lewang>
2011-09-17 21:26:09 +0800

    add force-eval-buffer.el

---
+++ force-eval-buffer.el
@@ -0,0 +1,25 @@
+;; this evaluates a buffer, but changing all variable definition to setq to
+;; force reevaluation.  It's useful if you want to reset to a library's
+;; defaults without restarting Emacs
+
+;;;;;;;;;;;; these are stupid alternatives I tried before
+
+(defmacro replacement-def (sym val &rest args)
+  `(setq ,sym ,val))
+
+;;;###autoload
+(defun force-eval-buffer (&optional buf)
+  (interactive)
+  (eval-buffer buf)                         ; define variables properly at least once
+  (let ((func-alist (mapcar (lambda (func)
+                              (cons func (symbol-function func)))
+                            '(defvar defcustom))))
+    (unwind-protect
+        (progn
+          (mapc (lambda (func-cons)
+                  (fset (car func-cons) (symbol-function 'replacement-def)))
+                func-alist)
+          (eval-buffer buf))
+      (mapcar (lambda (func-cons)
+                (fset (car func-cons) (cdr func-cons)))
+              func-alist))))
