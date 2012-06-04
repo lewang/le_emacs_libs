b0318a651903c5b92378edb01b896b615820cfca
Le Wang <le.wang@agworld.com.au>
2012-06-05 00:28:04 +0800

    make force-eval work on regions or buffers

--- force-eval.el
+++ force-eval.el
@@ -11,9 +11,13 @@
   `(setq ,sym ,val))
 
 ;;;###autoload
-(defun force-eval-buffer (&optional buf)
-  (interactive)
-  (eval-buffer buf)                         ; define variables properly at least once
+(defun force-eval (beg end)
+  "force eval region, if no region is active, then use buffer"
+  (interactive (if (use-region-p)
+                   (list (region-beginning)
+                         (region-end))
+                 (point-min) (point-max)))
+  (eval-region beg end)                         ; define variables properly at least once
   (let ((func-alist (mapcar (lambda (func)
                               (cons func (symbol-function func)))
                             '(defvar defcustom))))
@@ -22,7 +26,7 @@
           (mapc (lambda (func-cons)
                   (fset (car func-cons) (symbol-function 'replacement-def)))
                 func-alist)
-          (eval-buffer buf))
+          (eval-region beg end))
       (mapcar (lambda (func-cons)
                 (fset (car func-cons) (cdr func-cons)))
               func-alist))))
